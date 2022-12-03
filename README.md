9d6da258cbb5c840d242384113b4a23311ce0726
Cesar Felipe <cesar.felipe150@gmail.com>
2022-12-03 20:52:20 -0300

    input and output data in localStorage

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # alura-armazenando-na-web
+
+Os tipos de dados armazenados no localStorage não devem ser considerados sensíveis, de acordo com a LGPD (Lei Geral de Proteção de Dados). Isso ocorre, pois ele não possui nenhuma camada de proteção, e os dados podem ser acessados facilmente por terceiros. Dados considerados sensíveis, devem ser armazenados em Cookies.
+
--- js/main.js
+++ js/main.js
@@ -1,23 +1,60 @@
 const formulario = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
-formulario.addEventListener('submit', (evento)=>{
+itens.forEach(elemento => {
+    criaElemento(elemento);
+});
+
+formulario.addEventListener('submit', (evento) => {
     evento.preventDefault();
 
-    criaElemento(evento.target.elements['nome'].value,evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+
+    const itemExiste = itens.find((elemento) => { return elemento.nome === nome.value })
+
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value,
+    }
+
+    if (itemExiste) {
+        itemAtual.id = itemExiste.id;
+
+        atualizaElemento(itemAtual);
+
+        itens[itemExiste.id] = itemAtual
+    } else {
+        itemAtual.id = itens.length;
+
+        criaElemento(itemAtual);
+
+        itens.push(itemAtual)
+    }
+
+
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+
+    nome.value = "";
+    quantidade.value = "";
 
 })
 
-function criaElemento(nome, quantidade){
+function criaElemento(ObjItem) {
     const novoItem = document.createElement('li');
     novoItem.classList.add('item');
 
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade;
-
+    numeroItem.innerHTML = ObjItem.quantidade;
+    numeroItem.dataset.id = ObjItem.id;
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
+    novoItem.innerHTML += ObjItem.nome;
 
     lista.appendChild(novoItem);
+}
 
+function atualizaElemento(ObjItem){
+    document.querySelector("[data-id='"+ObjItem.id+"']").innerHTML = ObjItem.quantidade;
 }
